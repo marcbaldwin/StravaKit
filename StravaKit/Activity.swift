b7db423685af9bd652499dda1ef57491eb378fd8
marcbaldwin <marc.baldwin88@gmail.com>
2016-01-31 18:55:27 +0000

    Make Activity properties public

--- StravaKit/Activity.swift
+++ StravaKit/Activity.swift
@@ -1,37 +1,39 @@
 public class Activity {
 
-    var id: Int!
-    var name: String!
-    var distance: Distance!
-    var movingTime: Time!
-    var elapsedTime: Time!
-    var totalElevationGain: Distance!
+    public var id: Int!
+    public var name: String!
+    public var distance: Distance!
+    public var movingTime: Duration!
+    public var elapsedTime: Duration!
+    public var totalElevationGain: Distance!
 
-    var averageSpeed: Speed!
-    var maxSpeed: Speed!
+    public var averageSpeed: Speed!
+    public var maxSpeed: Speed!
 
-    var type: String!
-    var startDate: NSDate!
-    var localTimeZone: NSTimeZone!
+    public var type: String!
+    public var startDate: NSDate!
+    public var localTimeZone: NSTimeZone!
 
-    var startCoordinate: Coordinate!
-    var endCoordinate: Coordinate!
+    public var startCoordinate: Coordinate!
+    public var endCoordinate: Coordinate!
 
-    var achievementCount: Int!
-    var kudosCount: Int!
-    var commentCount: Int!
-    var athleteCount: Int!
-    var photoCount: Int!
-    var totalPhotoCount: Int!
-    var isStaticTrainer: Bool!
-    var isCommute: Bool!
-    var isManual: Bool!
-    var isPrivate: Bool!
-    var isFlagged: Bool!
-    var averageWatts: Float!
-    var weightedAverageWatts: Float!
-    var kilojoules: Float!
-    var deviceWatts: Bool!
-    var averageHeartRate: Float!
-    var maxHeartRate: Float!
+    public var achievementCount: Int!
+    public var kudosCount: Int!
+    public var commentCount: Int!
+    public var athleteCount: Int!
+    public var photoCount: Int!
+    public var totalPhotoCount: Int!
+    public var isStaticTrainer: Bool!
+    public var isCommute: Bool!
+    public var isManual: Bool!
+    public var isPrivate: Bool!
+    public var isFlagged: Bool!
+    public var averageWatts: Float!
+    public var weightedAverageWatts: Float!
+    public var kilojoules: Float!
+    public var deviceWatts: Bool!
+    public var averageHeartRate: Float!
+    public var maxHeartRate: Float!
+
+    public init() {}
 }
