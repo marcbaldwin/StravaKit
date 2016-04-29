b768075d41ccfc7aa7aaf3effbc712c1b11149f3
marcbaldwin <marc.baldwin88@gmail.com>
2016-04-29 13:24:39 +0200

    Replaces Range with ..< operator

--- StravaKit/StringExtensions.swift
+++ StravaKit/StringExtensions.swift
@@ -11,7 +11,7 @@ extension String {
     }
 
     subscript (r: Range<Int>) -> String {
-        return substringWithRange(Range(start: startIndex.advancedBy(r.startIndex), end: startIndex.advancedBy(r.endIndex)))
+        return substringWithRange(startIndex.advancedBy(r.startIndex) ..< startIndex.advancedBy(r.endIndex))
     }
 
     var fileURL: NSURL? {
