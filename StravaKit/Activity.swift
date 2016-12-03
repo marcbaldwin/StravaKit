a85b2b4131f2d0504c82d4e720c877429745b2a3
marcbaldwin <marc.baldwin88@gmail.com>
2016-12-03 22:29:45 +0000

    Adds public initializer to Activity

--- StravaKit/Activity.swift
+++ StravaKit/Activity.swift
@@ -17,6 +17,43 @@ public struct Activity {
     public let isManual: Bool
     public let averageHeartRate: Float
     public let maxHeartRate: Float
+
+    public init(
+        id: Int,
+        name: String,
+        distance: Distance,
+        movingTime: Duration,
+        elapsedTime: Duration,
+        totalElevationGain: Distance,
+        averageSpeed: Speed,
+        maxSpeed: Speed,
+        type: String,
+        startDate: String,
+        localTimeZone: String,
+        polyline: String,
+        isStaticTrainer: Bool,
+        isCommute: Bool,
+        isManual: Bool,
+        averageHeartRate: Float,
+        maxHeartRate: Float) {
+        self.id = id
+        self.name = name
+        self.distance = distance
+        self.movingTime = movingTime
+        self.elapsedTime = elapsedTime
+        self.totalElevationGain = totalElevationGain
+        self.averageSpeed = averageSpeed
+        self.maxSpeed = maxSpeed
+        self.type = type
+        self.startDate = startDate
+        self.localTimeZone = localTimeZone
+        self.polyline = polyline
+        self.isStaticTrainer = isStaticTrainer
+        self.isCommute = isCommute
+        self.isManual = isManual
+        self.averageHeartRate = averageHeartRate
+        self.maxHeartRate = maxHeartRate
+    }
 }
 
 extension Activity: Equatable { }
