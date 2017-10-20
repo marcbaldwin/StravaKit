164b625c76f580c732aa5a697fedd7ef361ad16d
Marc Baldwin <marc.baldwin88@gmail.com>
2017-10-20 12:50:00 +0100

    Add athlete routes endpoint

--- StravaKit/StravaClient.swift
+++ StravaKit/StravaClient.swift
@@ -2,13 +2,13 @@ import Moya
 import Alamofire
 
 public enum StravaQuery {
-    case page(Int)
     case before(Int)
     case after(Int)
 }
 
 public enum StravaApi {
-    case athleteActivities(accessToken: String, query: [StravaQuery], size: Int)
+    case athleteActivities(accessToken: String, query: [StravaQuery], page: Int, pageSize: Int)
+    case athleteRoutes(accessToken: String, page: Int, pageSize: Int)
 }
 
 extension StravaApi: TargetType {
@@ -20,6 +20,7 @@ extension StravaApi: TargetType {
     public var path: String {
         switch self {
         case .athleteActivities: return "athlete/activities"
+        case .athleteRoutes: return "athlete/routes"
         }
     }
 
@@ -29,13 +30,16 @@ extension StravaApi: TargetType {
 
     public var task: Task {
         switch self {
-        case let .athleteActivities(accessToken, query, size):
-            var params = [String : Any]()
-            params["access_token"] = accessToken
-            params["per_page"] = size
+
+        case let .athleteActivities(accessToken, query, page, pageSize):
+            var params = requestParameters(accessToken: accessToken, page: page, pageSize: pageSize)
             for (key, value) in query.flatMap({ $0.parameters }) {
                 params[key] = value
             }
+            return .requestParameters(parameters: params, encoding: URLEncoding.default)
+
+        case let .athleteRoutes(accessToken, page, pageSize):
+            let params = requestParameters(accessToken: accessToken, page: page, pageSize: pageSize)
             return .requestParameters(parameters: params, encoding: URLEncoding.default)
         }
     }
@@ -47,13 +51,20 @@ extension StravaApi: TargetType {
     public var sampleData: Data {
         return "".data(using: .utf8)!
     }
+
+    private func requestParameters(accessToken: String, page: Int, pageSize: Int) -> [String : Any] {
+        return [
+            "access_token" : accessToken,
+            "page" : page,
+            "per_page" : pageSize,
+        ]
+    }
 }
 
 extension StravaQuery {
 
     public var parameters: [String : Any] {
         switch self {
-        case let .page(page): return ["page" : page]
         case let .before(seconds): return ["before" : seconds]
         case let .after(seconds): return ["after" : seconds]
         }
