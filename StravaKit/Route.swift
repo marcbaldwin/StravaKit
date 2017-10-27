05f114338be7764fd5cbab1aff6739adf48ba69b
Marc Baldwin <marc.baldwin88@gmail.com>
2017-10-27 09:36:11 +0100

    Fix formatting

--- StravaKit/Route.swift
+++ StravaKit/Route.swift
@@ -2,7 +2,7 @@
 public struct Route: Codable {
 
     public let id: Int
-	public let name: String
-	public let description: String
+    public let name: String
+    public let description: String
     public let map: Map
 }
