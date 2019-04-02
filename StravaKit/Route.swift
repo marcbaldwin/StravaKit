065ccf218d8d8508f7dc9ee58427e8cb0aff77b4
Marc Baldwin <marc.baldwin88@gmail.com>
2019-04-02 11:46:22 +0100

    Make Route.description optional

--- StravaKit/Route.swift
+++ StravaKit/Route.swift
@@ -3,6 +3,6 @@ public struct Route: Codable, Equatable {
 
     public let id: Int64
     public let name: String
-    public let description: String
+    public let description: String?
     public let map: Map
 }
