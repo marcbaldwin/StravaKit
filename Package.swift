059ceef0db66e94c921f07e87b494b257fd7c41a
Marc Baldwin <marc.baldwin88@gmail.com>
2024-02-21 10:30:51 +0000

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "StravaKit",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+      .library(name: "StravaKit", targets: ["StravaKit"])
+    ],
+    dependencies: [
+      .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
+      .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.8.1"))
+    ],
+    targets: [
+        .target(
+            name: "StravaKit",
+            dependencies: [
+              "RxSwift",
+              "Alamofire"
+            ],
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
