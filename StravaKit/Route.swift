44317d3bdf1ae4236cf7ca6f8b45206ca4fda018
Kyle Goslan <kylegoslan@me.com>
2024-03-04 12:21:38 +0000

    Make Identifiable

--- StravaKit/Route.swift
+++ StravaKit/Route.swift
@@ -1,4 +1,4 @@
-public struct Route: Codable, Equatable, Hashable {
+public struct Route: Codable, Equatable, Hashable, Identifiable {
     public let id: Int64
     public let name: String
     public let description: String?
